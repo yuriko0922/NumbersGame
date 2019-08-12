f192ecb2a981793f164f9e7b6d6f4bca981ec3bb
“yuriko0922” <yrk922@gmail.com>
2019-08-12 15:52:45 +0900

    もう少し

--- NumbersGame/ViewController.swift
+++ NumbersGame/ViewController.swift
@@ -9,14 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     //答えが表示されるラベル
     @IBOutlet weak var answerabel: UILabel!
     
     //答えを入力するテキストフィールド
     @IBOutlet weak var answerTextField: UITextField!
     
-   
+    
     //回答履歴が書かれるところ
     @IBOutlet weak var rirekiText: UITextView!
     
@@ -24,10 +24,52 @@ class ViewController: UIViewController {
     var oknum: Int = 0
     
     //回答回数のカウント
-    var count: Int = 1
+    var count: Int = 0
     
     //ランダムに数を生成
     var lannum: Int = Int.random(in: 1...100)
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //テキストビューに入力できないようにする
+        rirekiText.isUserInteractionEnabled = false
+        
+    }
+    
+    //決定ボタン
+    @IBAction func okbutton(_ sender: Any) {
+        
+        func standertAlert(message: String) {
+            // アラートの作成
+            let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+            // アラートのアクション（ボタン部分の定義）
+            let close = UIAlertAction(title: "閉じる", style: .cancel, handler: nil)
+            // 作成した alert に閉じるボタンを追加
+            alert.addAction(close)
+            // アラートを表示する
+            present(alert, animated: true, completion: nil)
+        }
+        
+        
+        
+        func errorAlert() {
+            // アラート作る
+            let alerterror = UIAlertController(title: "エラー", message: "「1〜100」の数字を入れてください。", preferredStyle: .alert)
+            // アラートのアクション（ボタン部分の定義）
+            let ok = UIAlertAction(title: "oK", style: .cancel, handler: nil)
+            // alerterrornに閉じるボタンつける
+            alerterror.addAction(ok)
+            // アラートの表示
+            present(alerterror, animated: true, completion: nil)
+            
+        }
+       
+    
+    
+    
+    
     
     
     
@@ -47,43 +89,42 @@ class ViewController: UIViewController {
             
             if answerNum < lannum {
                 count += 1
+                standertAlert(message: "答えは\(answerNum)より高い値です。")
+                rirekiText.text = rirekiText.text + "\n[\(count)回目]答えは\(answerNum)より高い値です。"
                 
+            } else if answerNum > lannum {
+                count += 1
+                standertAlert(message: "答えは\(answerNum)より低い値です。")
+                rirekiText.text = rirekiText.text + "\n[\(count)回目]答えは\(answerNum)より低い値です。"
+                
+            } else if answerNum == lannum {
+                count += 1
+                standertAlert(message: "\(count)回目に正解しました！\n数字をリセットしました！")
+                rirekiText.text = rirekiText.text + "[正解]答えは\(lannum)でした。\n"
+                
+                
+                //リセット
+                answerabel.text = "??"
+                count = 0
+                lannum = Int.random(in: 1...100)
+                
+            } else {
+                errorAlert()
             }
-                
-            
         }
-    }
-
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        //テキストビューに入力できないようにする
-        rirekiText.isUserInteractionEnabled = false
+        
+        
         
     }
-
-
-    //決定ボタン
-    @IBAction func okbutton(_ sender: Any) {
-        
-        func errorAlert() {
-            // アラート作る
-            let alerterror = UIAlertController(title: "エラー", message: "「1〜100」の数字を入れてください。", preferredStyle: .alert)
-            // アラートのアクション（ボタン部分の定義）
-            let ok = UIAlertAction(title: "oK", style: .cancel, handler: nil)
-            // alerterrornに閉じるボタンつける
-            alerterror.addAction(ok)
-            // アラートの表示
-            present(alerterror, animated: true, completion: nil)
-            
-        }
-      //チェック呼び出し
-    checkAnswer()
+    
+    
+        //チェック呼び出し
+        checkAnswer()
+    
+    
     }
     
-    
+   
     
     
     
