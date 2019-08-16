58fb96d8f1c3d99e6d51771c36b8ef5a44aed8d1
“yuriko0922” <yrk922@gmail.com>
2019-08-16 13:15:26 +0900

    最終修正

--- NumbersGame/ViewController.swift
+++ NumbersGame/ViewController.swift
@@ -10,83 +10,83 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //答えが表示されるラベル
+    // 答えが表示されるラベル
     @IBOutlet weak var answerabel: UILabel!
     
-    //答えを入力するテキストフィールド
+    // 答えを入力するテキストフィールド
     @IBOutlet weak var answerTextField: UITextField!
     
-    //回答履歴が書かれるところ
+    // 回答履歴が書かれるところ
     @IBOutlet weak var rirekiText: UITextView!
     
-    //回答回数のカウント
+    // 回答回数のカウント
     var count: Int = 0
     
-    //ランダムに数を生成
+    // ランダムに数を生成
     var lanNum: Int = Int.random(in: 1...100)
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    //決定ボタン
+    // 決定ボタン
     @IBAction func okbutton(_ sender: Any) {
         checkAnswer()
     }
     
-    //回答を確認する関数
+    // 回答を確認する関数
     func checkAnswer() {
         if let answerNum = Int(answerTextField.text!) {
-            //１以上１００以下の時・・
+            // １以上１００以下の時・・
             if answerNum >= 1 && answerNum <= 100 {
             } else {
                 errorAlert()
                 answerTextField.text = ""
                 return
             }
-            //入力したテキストをラベルに表示する処理
+            // 入力したテキストをラベルに表示する処理
             answerabel.text = answerTextField.text
-            //入力したテキスト消す
+            // 入力したテキスト消す
             answerTextField.text = ""
             
-            //ここから、入力した数字が高いか低いかの精査に入ります
-            //回答が正解の数より低い場合
+            // ここから、入力した数字が高いか低いかの精査に入ります
+            // 回答が正解の数より低い場合
             if answerNum < lanNum {
-                //回答カウント１増やす
+                // 回答カウント１増やす
                 count += 1
-                //アラート呼んで、高いよっていうアラート表示させます
+                // アラート呼んで、高いよっていうアラート表示させます
                 standertAlert(message: "答えは\(answerNum)より高い値です。")
-                //何回目かと、回答の結果を下の編集不可にしたエリアに表示させる処理
+                // 何回目かと、回答の結果を下の編集不可にしたエリアに表示させる処理
                 rirekiText.text = rirekiText.text + "\n[\(count)回目]答えは\(answerNum)より高い値です。"
                 
-                //回答が正解の数より高い場合
+                // 回答が正解の数より高い場合
             } else if answerNum > lanNum {
-                //回答カウント１増やす
+                // 回答カウント１増やす
                 count += 1
-                //アラート呼んで、低いよっていうアラート表示させます
+                // アラート呼んで、低いよっていうアラート表示させます
                 standertAlert(message: "答えは\(answerNum)より低い値です。")
-                //何回目かと、回答の結果を下の編集不可にしたエリアに表示させる処理
-                rirekiText.text = rirekiText.text + "\n[\(count)回目]答えは\(answerNum)より低い値です。"
+                // 何回目かと、回答の結果を下の編集不可にしたエリアに表示させる処理
+                rirekiText.text += "\n[\(count)回目]答えは\(answerNum)より低い値です。"
                 
-                //回答が正解と一致した場合
+                // 回答が正解と一致した場合
             } else if answerNum == lanNum {
-                //回答カウント１増やす
+                // 回答カウント１増やす
                 count += 1
-                //アラート表示
+                // アラート表示
                 standertAlert(message: "\(count)回目に正解しました！\n数字をリセットしました！")
-                //編集不可にしたエリアに正解履歴を残す
-                rirekiText.text = rirekiText.text + "\n[正解]答えは\(lanNum)でした。"
+                // 編集不可にしたエリアに正解履歴を残す
+                rirekiText.text += "\n[正解]答えは\(lanNum)でした。"
                 
-                //リセット
+                // リセット
                 answerabel.text = "??"
                 count = 0
                 lanNum = Int.random(in: 1...100)
             } else {
-                //1~100以外の数字を入力した時にエラーアラート呼びだされる処理
+                // 1~100以外の数字を入力した時にエラーアラート呼びだされる処理
                 errorAlert()
             }
         } else {
-            //それ以外のエラー（文字とか。てかそれ以外って文字しかなくね？笑）
+            // それ以外のエラー
             errorAlert()
         }
     }
